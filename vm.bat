d5b6da47069248aafc154d2cf567fedbf4312cbb
ioanlongjing <ioan.snickersnee@gmail.com>
2017-06-30 10:30:39 +0800

    修正跨日與很多bug, 並新增 log 功能

--- vm.bat
+++ vm.bat
@@ -1,12 +1,14 @@
-::set vmrun="C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe"
-::set vm1="C:\Users\corec\Documents\Virtual Machines\Windows 10 x64\Windows 10 x64.vmx"
-::set vm2="C:\Users\corec\Documents\Virtual Machines\Windows10_Clone\Windows10_Clone.vmx"
+@echo off
+set vmrun="C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe"
+set vm1="C:\Users\corec\Documents\Virtual Machines\Windows 10 x64\Windows 10 x64.vmx"
+set vm2="C:\Users\corec\Documents\Virtual Machines\Windows10_Clone\Windows10_Clone.vmx"
 set vmlog="C:\Users\corec\Desktop\vmlog.txt"
 set vm_run="2"
-echo %time%
+set rest_period=1
 
-echo "Start time" >> %vmlog%
-echo %time >> %vmlog%
+echo "start the vmrun" >> %vmlog%
+echo %time%            >> %vmlog%
+echo "===============" >> %vmlog%
 
 set /A _lasttime=%time:~0,2%*3600^
             +%time:~3,1%*10*60^
@@ -28,81 +30,135 @@ echo %_nowtime%
 echo %_lasttime%
 echo %_elapsed%
 
-:: if worktime
-if %time:~0,2% lss 19 (
-if %time:~0,2% geq 8 (
+set now_time=%time:~0,2%
+
+if %now_time% lss 19 (
+    if %now_time% geq 8 (
     %vmrun% suspend %vm1% 
     %vmrun% suspend %vm2% 
+    echo "Both have suspend" >> %vmlog%
+    set rest_period=1
     )
 )
 
-:: vm switch time
-:: work for 7 
-if %time:~0,2% geq 19 (
+:: work for 19 to 24
+if %now_time% geq 19 (
+    if %rest_period% EQU 1 (
+        set rest_period=2
+        %vmrun% suspend %vm2%
+        %vmrun% start %vm1% 
+        echo %time% >>  %vmlog%
+        echo "It is run 19 to 24. Stap A" >> %vmlog% 
+        echo "start vm1 suspend vm2" >> %vmlog%
+        echo "=====================" >> %vmlog%
 
-    if %_elapsed% gtr 16200 
-    (
-        set _lasttime = %_nowtime%
+        set _lasttime=%_nowtime%
+    )
+    if %_elapsed% gtr 900 (
+        set _lasttime=%_nowtime%
         if %vm_run% == "1" (
-            %vmrun% suspend %vm1% 
-            %vmrun% start %vm2%
+            %vmrun% suspend %vm2%
+            %vmrun% start %vm1%
+            echo %time% >>  %vmlog%
+            echo "It is run 19 to 24. Stap B" >> %vmlog% 
+            echo "start vm1 suspend vm2" >> %vmlog%
+            echo "=====================" >> %vmlog%
+
             set vm_run="2"
         ) else (
         if %vm_run% == "2" (
-            %vmrun% suspend %vm2% 
+            %vmrun% suspend %vm1%
+            %vmrun% start %vm2%
+            echo %time% >>  %vmlog%
+            echo "It is run 19 to 24. Stap C" >> %vmlog% 
+            echo "start vm2 suspend vm1" >> %vmlog%
+            echo "=====================" >> %vmlog%
+
+            set vm_run="1"
+        )
+        )
+        echo %_lasttime%
+    )
+
+    if %_elapsed% leq 0 (
+        set _lasttime = %_nowtime%
+        if %vm_run% == "1" (
+            %vmrun% suspend %vm2%
             %vmrun% start %vm1%
+            echo %time% >>  %vmlog%
+            echo "It is run 19 to 24. Stap D" >> %vmlog% 
+            echo "start vm1 suspend vm2" >> %vmlog%
+            echo "=====================" >> %vmlog%
+
+            set vm_run="2"
+        ) else (
+        if %vm_run% == "2" (
+            %vmrun% suspend %vm1%
+            %vmrun% start %vm2%
+            echo %time% >>  %vmlog%
+            echo "It is run 19 to 24. Stap E" >> %vmlog% 
+            echo "start vm2 suspend vm1" >> %vmlog%
+            echo "=====================" >> %vmlog%
+
+            set vm_run="1"
+        )
+        )
+    )
+)
+
+:: work for 00 to 08
+if %now_time% lss 8 (
+    echo "work for 00 to 08"
+    if %_elapsed% gtr 900 (
+        set _lasttime=%_nowtime%
+        if %vm_run% == "1" (
+            %vmrun% suspend %vm2%
+            %vmrun% start %vm1%
+            echo %time% >>  %vmlog%
+            echo "It is run 19 to 24. Stap F" >> %vmlog% 
+            echo "start vm1 suspend vm2" >> %vmlog%
+            echo "=====================" >> %vmlog%
+
+            set vm_run="2"
+        ) else (
+        if %vm_run% == "2" (
+            %vmrun% suspend %vm2%
+            %vmrun% start %vm1%
+            echo %time% >>  %vmlog%
+            echo "It is run 19 to 24. Stap G" >> %vmlog%
+            echo "start vm2 suspend vm1" >> %vmlog%
+            echo "=====================" >> %vmlog%
+
             set vm_run="1"
         )
         )
     )
 
     if %_elapsed% leq 0 (
-        set _lasttime = %_nowtime%
+        set _lasttime=%_nowtime%
         if %vm_run% == "1" (
-            %vmrun% suspend %vm1% 
+            %vmrun% suspend %vm1%
             %vmrun% start %vm2%
+            echo %time% >> %vmlog%
+            echo "It is run 19 to 24. Stap H" >> %vmlog% 
+            echo "start vm1 suspend vm2" >> %vmlog%
+            echo "=====================" >> %vmlog%
+
             set vm_run="2"
         ) else (
         if %vm_run% == "2" (
-            %vmrun% suspend %vm2% 
-            %vmrun% start %vm1%
+            %vmrun% suspend %vm1%
+            %vmrun% start %vm2%
+            echo %time% >> %vmlog%
+            echo "It is run 19 to 24. Stap I" >> %vmlog% 
+            echo "start vm2 suspend vm1" >> %vmlog%
+            echo "=====================" >> %vmlog%
+
             set vm_run="1"
         )
         )
     )
 )
 
-if %time:~0,2% lss 8 (
 
-    if %_elapsed% gtr 16200 
-    (
-        set _lasttime = %_nowtime%
-        if %vm_run% == "1" (
-            %vmrun% suspend %vm1% 
-            %vmrun% start %vm2%
-            set vm_run="2"
-        ) else (
-        if %vm_run% == "2" (
-            %vmrun% suspend %vm2% 
-            %vmrun% start %vm1%
-            set vm_run="1"
-        )
-        )
-    )
-
-    if %_elapsed% leq 0 (
-        set _lasttime = %_nowtime%
-        if %vm_run% == "1" (
-            %vmrun% suspend %vm1% 
-            %vmrun% start %vm2%
-            set vm_run="2"
-        ) else (
-        if %vm_run% == "2" (
-            %vmrun% suspend %vm2% 
-            %vmrun% start %vm1%
-            set vm_run="1"
-        )
-        )
-    )
-)
 goto loop
